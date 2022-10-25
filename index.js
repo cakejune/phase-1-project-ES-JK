c93cab74dc9859e21844542709452de336403045
jake <tyq444@gmail.com>
2022-10-24 21:39:50 -0400

    added a button that checks if we have enough submissions to make a crossword.

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const emailField = document.querySelector('#emailvalue');
 const phoneField = document.querySelector('#phonevalue');
 const userForm = document.querySelector('form#totalform');
 const newPersonData = document.querySelector('#newPerson');
+const generateButton = document.querySelector('#generate');
+
+function main() {
+    fetch('http://localhost:3000/people')
+    .then(res=> res.json())
+    .then(peopleList => {
+        console.log(peopleList.length);
+    })
+}
+
+main();
 userForm.addEventListener('submit', (e)=> {
     e.preventDefault();
     // console.log(nameField.value);
@@ -30,3 +41,20 @@ userForm.addEventListener('submit', (e)=> {
         newPersonData.append(nameInList);
     })
 })
+generateButton.addEventListener('click', (e)=>{
+    fetch('http://localhost:3000/people')
+    .then(res=>res.json())
+    .then(peopleList => {
+        console.log(`Submission Count: ${peopleList.length}.`);
+        if(peopleList.length >= 10) {
+            const newCrossword = document.createElement('table');
+            const crosswordPicture = document.createElement('img');
+            const buttonContainerSpan = document.querySelector('#buttonContainerSpan');
+            crosswordPicture.src = "https://umbc.edu/wp-content/uploads/2021/05/crossword.jpg";
+            buttonContainerSpan.append(crosswordPicture);
+        }
+        else {
+            console.log('Not enough submissions');
+        }
+    })
+});
