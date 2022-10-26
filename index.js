0851d006baa03f09526bca54aa081b19f58ec420
cakejune <tyq444@gmail.com>
2022-10-26 09:44:24 -0400

    organized our data and wrote cleaner functions. night 1 on the UES

--- index.js
+++ index.js
@@ -1,26 +1,74 @@
-
 const userForm = document.querySelector("form#totalform");
 const newPersonData = document.querySelector("#newPerson");
 const generateButton = document.querySelector("#generate");
 const formDivContainer = document.querySelector("div.form-style-3");
 //lets grab our answers on our form
 const nameField = document.querySelector("#namevalue");
-const q1Answer = document.querySelector('input#q1value');
-const q2Answer = document.querySelector('input#q2value');
-const q3Answer = document.querySelector('select.select-field');
-const quizForm = document.querySelector('form#quizForm');
-const quizQ1Label = document.querySelector('#quizQ1');
+const q1Answer = document.querySelector("input#q1value");
+const q2Answer = document.querySelector("input#q2value");
+const q3Answer = document.querySelector("select.select-field");
+const quizForm = document.querySelector("form#quizForm");
+const quizQ1Label = document.querySelector("#quizQ1"); //aray of questions
+const questionUl = document.querySelector('#questions-list');
+
+async function main() {
+  const quiz = await getQuiz();
+  generateButton.addEventListener('click', () => {
+    renderQuiz(quiz);
+  })
+    }
 
 
-function main() {
-  fetch("http://localhost:3000/people")
-    .then((res) => res.json())
-    .then((peopleList) => {
-      console.log(peopleList.length);
-    });
+async function getQuiz() {
+  const resp = await fetch("http://localhost:3000/people");
+  const submissions = await resp.json();
+  const QnAs = [];
+  for (const person of submissions) {
+    QnAs.push(
+      {
+        question: `What is ${person.name}'s favorite color?`,
+        answer: person.color,
+      },
+      {
+        question: `What is ${person.name}'s favorite animal?`,
+        answer: person.animal,
+      },
+      {         
+        question: `Who is ${person.name}'s favorite teacher?`,
+        answer: person.teacher,
+      }
+    );
+  }
+  return QnAs;
 }
 
+function renderQuiz(arrayOfQuestionsAndAnswers) {
+    if(arrayOfQuestionsAndAnswers.length >= 25) {
+    arrayOfQuestionsAndAnswers.forEach((qna)=>{
+        const question = document.createElement('li');
+        const questionInput = document.createElement('input');
+        question.textContent = qna.question;
+        questionUl.appendChild(question, questionInput);
+        
+    });
+}
+else {
+    console.log("Not enough submissions.");
+}
+  // create questions based on the data
+//   for (const qnapair of QnAs){
+//     //create element
+//     //make element's contents = question in the object
+//     //assign this to a constant
+//   }
+
+}
+// function writeQuestions(person, key) {
+
+// }
+
 main();
+
 userForm.addEventListener("submit", (e) => {
   e.preventDefault();
   fetch("http://localhost:3000/people", {
@@ -44,68 +92,63 @@ userForm.addEventListener("submit", (e) => {
       newPersonData.append(nameInList);
     });
 });
-generateButton.addEventListener("click", (e) => {
-  fetch("http://localhost:3000/people")
-    .then((res) => res.json())
-    .then((peopleList) => {
-      console.log(`Submission Count: ${peopleList.length}.`);
-      if (peopleList.length >= 2) {
 
-        //THIS IS WHERE WE BUILD THE QUIZ ELISA AND CONNOR
-        formDivContainer.style.display = "none";
-        const newCrossword = document.createElement("table");
-        const crosswordPicture = document.createElement("img");
-        const buttonContainerSpan = document.querySelector(
-          "#buttonContainerSpan"
-        );
-        crosswordPicture.src =
-          "https://1.bp.blogspot.com/-XJJhIqBrLbg/Vd7TXWcxtmI/AAAAAAAAGdc/sK2h1V2_gOc/s1600/Jo%2Bquiz.PNG";
-        buttonContainerSpan.append(crosswordPicture);
+// generateButton.addEventListener("click", (e) => {
+  
+//     fetch("http://localhost:3000/people")
+//     .then((res) => res.json())
+//     .then((listOfPeople) => {
+//         if(listOfPeople.length >= 25) {
+//             renderQuiz(quiz);
+//         }
+//       //   if (peopleList.length >= 25) {
+//       //     //THIS IS WHERE WE BUILD THE QUIZ ELISA AND CONNOR
+//       //     formDivContainer.style.display = "none";
+//       //     const newCrossword = document.createElement("table");
+//       //     const crosswordPicture = document.createElement("img");
+//       //     const buttonContainerSpan = document.querySelector(
+//       //       "#buttonContainerSpan"
+//       //     );
+//       //     crosswordPicture.src =
+//       //       "https://1.bp.blogspot.com/-XJJhIqBrLbg/Vd7TXWcxtmI/AAAAAAAAGdc/sK2h1V2_gOc/s1600/Jo%2Bquiz.PNG";
+//       //     buttonContainerSpan.append(crosswordPicture);
 
-        // THIS IS WHERE WE BUILD THE QUIZ ^^^^
-      } else {
-        //TELL THE USER THAT WE CAN'T GENERATE A QUIZ UNTIL WE HAVE MORE SUBMISSIONS!
+//       //     // THIS IS WHERE WE BUILD THE QUIZ ^^^^
+//       //   } else {
+//       //     //TELL THE USER THAT WE CAN'T GENERATE A QUIZ UNTIL WE HAVE MORE SUBMISSIONS!
 
-        console.log("Not enough submissions");
-      }
-    //   quizQ1Label.textContent = `What is ${peopleList[0].name}'s favorite color?`;
-  });
+//       //     console.log("Not enough submissions");
+//       //   }
+//       for (let i = 0; i < listOfPeople.length; i++) {
+//         const newQuestion = document.createElement("li");
+//         newQuestion.setAttribute("data-id", listOfPeople[i].id);
+//         newQuestion.textContent = `What is ${listOfPeople[i].name}'s favorite color?`;
+//         console.log(newQuestion);
+//       }
+//       // Elisa's code below
+//       // questions.forEach((eachQuestion) => {
+//       //   for (let i = 0; i < listOfPeople.length; i++) {
+//       //     eachQuestion.setAttribute('data-id', listOfPeople[i].id);
+//       //     eachQuestion.setAttribute('group', 'populatedQuestion');
+//       //   }
+//       if (eachQuestion.id) {
+//         eachQuestion.textContent = `what is ${listOfPeople[0]}'s favorite color?`;
+//         eachQuestion.setAttribute("data-id", listOfPeople[0].id);
+//       } else {
+//         eachQuestion.style.display = "none";
+//       }
+//       // });
+//     });
+// });
 
-    quizForm.addEventListener('submit', (e) => { 
-    e.preventDefault();
-    fetch('http://localhost:3000/people')
-    .then(res=>res.json())
-    .then(listOfPeople => {
-      const questions = querySelectorAll('ul.quiz-questions');
-      questions.forEach((eachQuestion)=>{
-        for(let i = 0; i<listOfPeople.length; i++) {
-        eachQuestion.setAttribute('data-id',listOfPeople[i]+1);
-      };
-        if (eachQuestion.id){ 
-          eachQuestions.style.display = "none";     
-      eachQuestion.textContent = `what is ${listOfPeople[0]}'s favorite color?`
-      eachQuestion.setAttribute('data-id', listOfPeople[0].id);
-      }
-    })
-
-    })
-    })
-      
-     function checkAnswer(inputField, actualAnswer) {
-      //does q1 answer === listOfPeople[q1.getAttribute('data-id')].color?
-      Q1.id
-      score = score+1;
-      return `You got ${score} answers right`;
-      if ( q1_val.value === name )
-      return "Congratulations, your answer is correct" ;
-      else 
-     return  "Your answer is incorrect. Please try again" ;
-      } 
-    
-  })
-
-
-
+function checkAnswer(inputField, actualAnswer) {
+  //does q1 answer === listOfPeople[q1.getAttribute('data-id')].color?
+  Q1.id;
+  score = score + 1;
+  return `You got ${score} answers right`;
+  if (q1_val.value === name) return "Congratulations, your answer is correct";
+  else return "Your answer is incorrect. Please try again";
+}
 
 // function generateQuestions(arrayOfPeople) {
 //   return "a list of questions";
@@ -121,7 +164,6 @@ generateButton.addEventListener("click", (e) => {
 
 //   })
 // }
-
 
 // function showResult() {
 // }
